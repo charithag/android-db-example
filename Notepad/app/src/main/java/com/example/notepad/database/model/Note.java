0a504566d75a8849a52a673bc89169979d5833d8
charitha <charitha@entgra.io>
2019-11-24 16:44:41 +0530

    Add sample project

---
+++ Notepad/app/src/main/java/com/example/notepad/database/model/Note.java
@@ -0,0 +1,46 @@
+package com.example.notepad.database.model;
+
+public class Note {
+
+    public static final String TABLE_NAME = "notes";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOTE = "note";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+    private int id;
+    private String note;
+    private String timestamp;
+
+    public Note() {
+    }
+
+    public Note(int id, String note, String timestamp) {
+        this.id = id;
+        this.note = note;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
